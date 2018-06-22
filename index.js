8387e7352febcd5c3dd6704856a5eee98df41074
savannahgrasmick <grasmicksavannah@gmail.com>
2018-06-22 18:54:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your code in this file!
+
+
+
+function scuberGreetingForFeet(number) {
+  if (number <= 400) {
+  return "This one is on me!";
+} 
+else if (number > 2000 && number < 2500) {
+  return "I will gladly take your thirty bucks.";
+}
+else if (number > 2500) {
+  return "No can do.";
+}
+}
+
+function ternaryCheckCity(area) {
+  return (area === "NYC" ? "Ok, sounds good." : "No go.");
+}
+
+
+function switchOnCharmFromTip(expression) {
+  case "generous";
+  return = "Thank you so much"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
